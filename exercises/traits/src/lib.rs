7983625c4b67cdb3dcf7181ae605567e70336ba9
boochou <chaunguyen27423@gmail.com>
2023-07-17 18:02:52 +0700

    done

--- exercises/traits/src/lib.rs
+++ exercises/traits/src/lib.rs
@@ -13,10 +13,19 @@ trait Hello {
 //TODO 
 struct Student {}
 impl Hello for Student {
+    fn say_something(&self) -> String {
+        String::from("I'm a good student")
+    }
 }
 //TODO
 struct Teacher {}
 impl Hello for Teacher {
+    fn say_hi(&self) -> String {
+        String::from("Hi, I'm your new teacher")
+    }
+    fn say_something(&self) -> String {
+        String::from("I'm not a bad teacher")
+    }
 }
 
 
@@ -24,21 +33,38 @@ impl Hello for Teacher {
 // Make it compile in unit test for exercise 2
 // Hint: use #[derive]  for struct Point 
 // Run tests
+#[derive(Debug)]
 struct Point {
     x: i32,
     y: i32,
 }
-
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        if self.x!=other.x {
+            false
+        } else if self.y!=other.y {
+            false
+        } else {
+            true
+        }
+    }
+}
 
 // Exercise 3
 // Make it compile 
 // Implement `fn sum` with trait bound in two ways.
 // Run tests
 // Hint: Trait Bound
-fn sum<T>(x: T, y: T) -> T {
+fn sum<T: std::ops::Add<Output = T>>(x: T, y: T) -> T {
     x + y
 }
-
+// Cach 2
+// fn sum<T>(x: T, y: T) -> T
+// where
+//     T: std::ops::Add<Output = T>
+// {
+//     x + y
+// }
 
 // Exercise 4
 // Fix errors and implement
@@ -57,13 +83,13 @@ impl Foo for String {
 }
 
 // IMPLEMENT below with generics and parameters
-fn static_dispatch(x) {
-    todo!()
+fn static_dispatch <T: Foo> (x: T) {
+    println!("{}",x.method())
 }
 
 // Implement below with trait objects and parameters
-fn dynamic_dispatch(x) {
-    todo!()
+fn dynamic_dispatch(x: &dyn Foo) {
+    println!("{}",x.method())
 }
 
 // Exercise 5 
@@ -90,7 +116,7 @@ fn draw_with_box(x: Box<dyn Draw>) {
     x.draw();
 }
 
-fn draw_with_ref(x: __) {
+fn draw_with_ref(x: &dyn Draw) {
     x.draw();
 }
 
@@ -106,12 +132,26 @@ trait Container {
     fn is_empty(&self) -> bool;
 }
 
-struct Stack {
-    items: Vec<u8>,
+struct Stack <T>{
+    items: Vec<T>,
 }
 
 //TODO implement Container for Stack
+impl<T> Container for Stack<T> {
+    type Item = T;
 
+    fn insert(&mut self, item: Self::Item) {
+        self.items.push(item);
+    }
+
+    fn remove(&mut self) -> Option<Self::Item> {
+        self.items.pop()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+}
 
 
 #[cfg(test)]
@@ -161,7 +201,7 @@ mod tests {
         let y = 8u8;
     
         // Draw x.
-        draw_with_box(__);
+        draw_with_box(Box::new(x));
     
         // Draw y.
         draw_with_ref(&y);
